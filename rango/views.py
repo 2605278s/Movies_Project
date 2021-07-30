306385dc56c4fe641b432b2bdf27de890a6dccd9
Emilija Sapagovaite <emilija0414@Emilijas-MBP.lan>
2021-07-30 16:14:01 +0300

    Chapter 8 finally finished

--- rango/views.py
+++ rango/views.py
@@ -47,8 +47,9 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
-        else:print(form.errors)
+            return redirect('rango:index')
+        else:
+            print(form.errors)
 
     return render(request, 'rango/add_category.html', {'form': form})
 
@@ -59,7 +60,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect('rango:index')
 
     form = PageForm()
 
